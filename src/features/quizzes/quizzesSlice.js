8ae56c8ce0ddb8dc35434280d96fef4d1f72b62c
Bea <103050835+Beadko@users.noreply.github.com>
2022-10-09 13:22:38 +0700

    quizzesSlices with adQuiz action added

--- src/features/quizzes/quizzesSlice.js
+++ src/features/quizzes/quizzesSlice.js
@@ -1,1 +1,23 @@
-quizzesSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+export const quizzesSlice = createSlice({
+	name: 'quizzes',
+	initialState: {
+		quizzes:{}
+	},
+	reducers: {
+    	addQuiz:(state, action)=> {
+    		const { id, name, icon } = action.payload;
+      		state.quizzes[id] = {
+        		id: id,
+        		name: name,
+        		topicId: topicId,
+        		cardIds: []
+      		}
+		}
+	}
+});
+export const { addQuiz } = quizzesSlice.actions;
+export const selectQuizzes = (state) => state.quizzes.quizzes;
+export default quizzesSlice.reducer;
+
