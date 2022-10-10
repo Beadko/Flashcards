dcc1c4c456b2a5210e603bd950a60d94052f6196
Bea <103050835+Beadko@users.noreply.github.com>
2022-10-10 14:10:56 +0700

    add a new card action added to handleSubmit

--- src/components/NewQuizForm.js
+++ src/components/NewQuizForm.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
-import { selectTopics } from "../features/topics/topicsSlice.js"
+import { selectTopics } from "../features/topics/topicsSlice.js";
 import { useSelector, useDispatch } from 'react-redux';
-import { addQuizForTopic } from "../features/quizzes/quizzesSlice.js"
+import { addQuizForTopic } from "../features/quizzes/quizzesSlice.js";
+import { addCard } from "../features/cards/cardsSlice.js"
 
 export default function NewQuizForm() {
   const [name, setName] = useState("");
@@ -35,6 +36,11 @@ export default function NewQuizForm() {
 
     // create the new cards here and add each card's id to cardIds
     // create the new quiz here
+    cards.forEach((card) => {
+      let cardId = uuidv4();
+      cardIds.push(cardId);
+      dispatch(addCard({ ...card, id: cardId }));
+    });
 
     history.push(ROUTES.quizzesRoute());
 
